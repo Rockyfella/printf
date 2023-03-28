cbaab5aa7d2b505f1cd7570935545cd7f5d98137
Koketso Monamodi <110986933+Cookies933@users.noreply.github.com>
2023-03-28 11:45:02 +0200

    Update handlers.c

--- handlers.c
+++ handlers.c
@@ -1,117 +1,197 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
+
+unsigned char handle_flags(const char *flag, char *index);
+unsigned char handle_length(const char *modifier, char *index);
+int handle_width(va_list args, const char *modifier, char *index);
+int handle_precision(va_list args, const char *modifier, char *index);
+unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
+		unsigned char, int, int, unsigned char);
 
 /**
- * print_unsigned - Print an unsigned integer in decimal format
- * @n: The unsigned integer to print
+ * handle_flags - Matches flags with corresponding values.
+ * @flag: A pointer to a potential string of flags.
+ * @index: An index counter for the original format string.
  *
- * Return: The number of characters printed
+ * Return: If flag characters are matched - a corresponding value.
+ *         Otherwise - 0.
  */
-int print_unsigned(unsigned int n)
+unsigned char handle_flags(const char *flag, char *index)
 {
-	char buf[32];
-	int len;
+	int i, j;
+	unsigned char ret = 0;
+	flag_t flags[] = {
+		{'+', PLUS},
+		{' ', SPACE},
+		{'#', HASH},
+		{'0', ZERO},
+		{'-', NEG},
+		{0, 0}
+	};
 
-	len = sprintf(buf, "%u", n);
-	write(1, buf, len);
-
-	return (len);
-}
-
-/**
- * print_octal - Print an unsigned integer in octal format
- * @n: The unsigned integer to print
- *
- * Return: The number of characters printed
- */
-int print_octal(unsigned int n)
-{
-	char buf[32];
-	int len;
-
-	len = sprintf(buf, "%o", n);
-	write(1, buf, len);
-
-	return (len);
-}
-
-/**
- * print_hex - Print an unsigned integer in hexadecimal format
- * @n: The unsigned integer to print
- * @uppercase: Whether to use uppercase letters for the hexadecimal digits
- *
- * Return: The number of characters printed
- */
-int print_hex(unsigned int n, int uppercase)
-{
-	char buf[32];
-	int len;
-
-	if (uppercase)
-		len = sprintf(buf, "%X", n);
-	else
-		len = sprintf(buf, "%x", n);
-
-	write(1, buf, len);
-
-	return (len);
-}
-
-/**
- * _printf - Custom printf function
- * @format: The format string
- *
- * Return: The number of characters printed
- */
-int _printf(const char *format, ...)
-{
-	va_list args;
-	int i, len;
-	unsigned int u;
-
-	va_start(args, format);
-
-	len = 0;
-
-	for (i = 0; format[i]; i++)
+	for (i = 0; flag[i]; i++)
 	{
-		if (format[i] != '%')
+		for (j = 0; flags[j].flag != 0; j++)
 		{
-			write(1, &format[i], 1);
-			len++;
-			continue;
+			if (flag[i] == flags[j].flag)
+			{
+				(*index)++;
+				if (ret == 0)
+					ret = flags[j].value;
+				else
+					ret |= flags[j].value;
+				break;
+			}
 		}
-
-		i++;
-
-		switch (format[i])
-		{
-		case 'u':
-			u = va_arg(args, unsigned int);
-			len += print_unsigned(u);
+		if (flags[j].value == 0)
 			break;
-		case 'o':
-			u = va_arg(args, unsigned int);
-			len += print_octal(u);
-			break;
-		case 'x':
-			u = va_arg(args, unsigned int);
-			len += print_hex(u, 0);
-			break;
-		case 'X':
-			u = va_arg(args, unsigned int);
-			len += print_hex(u, 1);
-			break;
-		default:
-			write(1, &format[i - 1], 2);
-			len += 2;
-			break;
-		}
 	}
 
-	va_end(args);
+	return (ret);
+}
 
-	return (len);
+/**
+ * handle_length - Matches length modifiers with their corresponding value.
+ * @modifier: A pointer to a potential length modifier.
+ * @index: An index counter for the original format string.
+ *
+ * Return: If a lenth modifier is matched - its corresponding value.
+ *         Otherwise - 0.
+ */
+unsigned char handle_length(const char *modifier, char *index)
+{
+	if (*modifier == 'h')
+	{
+		(*index)++;
+		return (SHORT);
+	}
+
+	else if (*modifier == 'l')
+	{
+		(*index)++;
+		return (LONG);
+	}
+
+	return (0);
+}
+
+/**
+ * handle_width - Matches a width modifier with its corresponding value.
+ * @args: A va_list of arguments.
+ * @modifier: A pointer to a potential width modifier.
+ * @index: An index counter for the original format string.
+ *
+ * Return: If a width modifier is matched - its value.
+ *         Otherwise - 0.
+ */
+int handle_width(va_list args, const char *modifier, char *index)
+{
+	int value = 0;
+
+	while ((*modifier >= '0' && *modifier <= '9') || (*modifier == '*'))
+	{
+		(*index)++;
+
+		if (*modifier == '*')
+		{
+			value = va_arg(args, int);
+			if (value <= 0)
+				return (0);
+			return (value);
+		}
+
+		value *= 10;
+		value += (*modifier - '0');
+		modifier++;
+	}
+
+	return (value);
+}
+
+/**
+ * handle_precision - Matches a precision modifier with
+ *                    its corresponding value.
+ * @args: A va_list of arguments.
+ * @modifier: A pointer to a potential precision modifier.
+ * @index: An index counter for the original format string.
+ *
+ * Return: If a precision modifier is matched - its value.
+ *         If the precision modifier is empty, zero, or negative - 0.
+ *         Otherwise - -1.
+ */
+int handle_precision(va_list args, const char *modifier, char *index)
+{
+	int value = 0;
+
+	if (*modifier != '.')
+		return (-1);
+
+	modifier++;
+	(*index)++;
+
+	if ((*modifier <= '0' || *modifier > '9') &&
+		 *modifier != '*')
+	{
+		if (*modifier == '0')
+			(*index)++;
+		return (0);
+	}
+
+	while ((*modifier >= '0' && *modifier <= '9') ||
+		   (*modifier == '*'))
+	{
+		(*index)++;
+
+		if (*modifier == '*')
+		{
+			value = va_arg(args, int);
+			if (value <= 0)
+				return (0);
+			return (value);
+		}
+
+		value *= 10;
+		value += (*modifier - '0');
+		modifier++;
+	}
+
+	return (value);
+}
+
+/**
+ * handle_specifiers - Matches a conversion specifier with
+ *                     a corresponding conversion function.
+ * @specifier: A pointer to a potential conversion specifier.
+ *
+ * Return: If a conversion function is matched - a pointer to the function.
+ *         Otherwise - NULL.
+ */
+unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
+		unsigned char, int, int, unsigned char)
+{
+	int i;
+	converter_t converters[] = {
+		{'c', convert_c},
+		{'s', convert_s},
+		{'d', convert_di},
+		{'i', convert_di},
+		{'%', convert_percent},
+		{'b', convert_b},
+		{'u', convert_u},
+		{'o', convert_o},
+		{'x', convert_x},
+		{'X', convert_X},
+		{'S', convert_S},
+		{'p', convert_p},
+		{'r', convert_r},
+		{'R', convert_R},
+		{0, NULL}
+	};
+
+	for (i = 0; converters[i].func; i++)
+	{
+		if (converters[i].specifier == *specifier)
+			return (converters[i].func);
+	}
+
+	return (NULL);
 }
